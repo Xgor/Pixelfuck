cd3a6d090dd7a1f1fcbc1496d846200e2c5e5936
Unknown <real.xgor@hotmail.com>
2017-10-11 00:23:36 +0200

    Images can now be drawn

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 
+var canvasWidth = 256;
+var canvasHeight = 256;
+var imageWidth = 8;
+var imageHeight = 8;
+
+var pointer = 0;
+var memorySize = 3000;
+var currentPixel;
+var numbers = [];
+var loops = [];
+
+var canvas
+var context
+var pixelWidth 
+var pixelHeight
+
+//var checkerboard = "->.<.>.<.>.<.>.<..>.<.>.<.>.<.>.<>.<.>.<.>.<.>.<..>.<.>.<.>.<.>.<>.<.>.<.>.<.>.<..>.<.>.<.>.<.>.<>.<.>.<.>.<.>.<..>.<.>.<.>.<.>.<"
 
 function myFunction()
 {
-	var width = 64;
-	var height = 64;
-	var pointer = 0;
-	var numbers = [];
-	var loops = [];
+	currentPixel = 0
+
+	
+	pixelWidth = canvasWidth/imageWidth;
+	pixelHeight = canvasHeight/imageHeight;
+
+	canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0,0,canvas.width,canvas.height )
+
 	var text =document.querySelector(".text");
 	var textarea = document.querySelector(".txtarea");
 	text.innerHTML = "";
 
-	for (var i = 0; i < width*height; i++) {
+	for (var i = 0; i < memorySize; i++) {
 		numbers[i] = 0;
 	}
 
-	//for (var i = 0, len = textarea.value.length; i < len; i++) {
 	var index = 0;
 	while(index <textarea.value.length)
 	{
@@ -23,20 +44,25 @@ function myFunction()
 		{
 		case '<':
 			pointer--;
-			//console.log(pointer);
+			if(pointer< 0)
+				pointer += memorySize;
 			break;
 		case '>':
 			pointer++;
+			if(pointer == memorySize)
+				pointer -= memorySize;
 			break;
 		case '+':
 			numbers[pointer]++;
+			if(numbers[pointer]==256)
+				numbers[pointer]=0;
 			break;
 		case '-':
 			numbers[pointer]--;
 			break;
 		case '.':
 			text.innerHTML += String.fromCharCode(numbers[pointer]);
-
+			drawPixel(numbers[pointer])
 			break;
 		case ',':
 			break;
@@ -44,7 +70,6 @@ function myFunction()
 			loops.push(index);
 			break;
 		case ']':
-			console.log(numbers[pointer]);
 			if(numbers[pointer] != 0)
 			{
 				index = loops[loops.length-1];
@@ -56,9 +81,53 @@ function myFunction()
 			//
 			break;
 		}
+
 		//console.log(textarea.value.length);
 		index++;
 	}
-	//var c = document.getElementById("myCanvas");
-	//var ctx = c.getContext("2d");
 }
+
+
+
+function drawPixel(value)
+{
+//	var c = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+
+	var x = currentPixel%imageWidth
+	var y = Math.floor( currentPixel/imageWidth)
+	//ctx.fillStyle = 'rgb(' +getRed(value)+','+  getBlue(value)+','+ getGreen(value) +','+ getAlpha(value)+ ')'
+	ctx.fillStyle = 'rgb(' +getRed(value)+','+  getBlue(value)+','+ getGreen(value) + ')'
+	ctx.fillRect(x*pixelWidth, y*pixelHeight, pixelWidth, pixelHeight);
+
+	console.log('rgb(' +getRed(value)+','+  getBlue(value)+','+ getGreen(value) + ')')
+
+	currentPixel++;
+	if(currentPixel == imageWidth*imageHeight)
+		currentPixel = 0;
+}
+function getRed(value)
+{
+	return (3 & value) *64+getHighlight(value)
+}
+function getBlue(value)
+{
+	return ((12 & value)>>2) *64+getHighlight(value)
+}
+function getGreen(value)
+{
+	return ((48 & value)>>4) *64+getHighlight(value)
+}
+
+// function 
+
+function getHighlight(value)
+{
+	return ((196 & value)>>6) *16
+}
+
+//function getAlpha(value)
+//{
+//	return 255
+//	return ((196 & value)>>6) /4
+//}
